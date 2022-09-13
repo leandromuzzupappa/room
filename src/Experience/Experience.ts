a62348f92d29bce8853df51540c434bf2a7ac6d7
LMuzzupappa <leandro.muzzupappa@mediamonks.com>
2022-09-13 13:18:56 -0300

    Sizes class has been created

--- src/Experience/Experience.ts
+++ src/Experience/Experience.ts
@@ -1,9 +1,24 @@
-export default class Experience {
-  canvas;
+import * as Three from "three";
+import Sizes from "./utils/Sizes";
 
-  constructor(canvas: HTMLCanvasElement | null) {
+export default class Experience {
+  static instance: Experience;
+
+  canvas;
+  sizes;
+  scene: Three.Scene | undefined;
+
+  constructor(canvas: HTMLCanvasElement | null = null) {
+    if (Experience.instance) {
+      return Experience.instance;
+    }
+
+    Experience.instance = this;
     this.canvas = canvas;
 
-    console.log("dsadsa", this.canvas);
+    this.scene = new Three.Scene();
+    this.sizes = new Sizes();
+
+    console.log("sizes", this.sizes);
   }
 }
